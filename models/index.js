ed345234dd95c4a97d3e8dd5957e9ba7ee91892a
Rick welte <rktvpr@gmail.com>
2023-01-17 18:24:56 -0500

    added all models

---
+++ models/index.js
@@ -0,0 +1,4 @@
+const User = require('./user');
+const { Thought, Reaction } = require('./thought')
+
+module.exports = {User, Thought, Reaction}
---
+++ models/thought.js
@@ -0,0 +1,68 @@
+const { Schema, model } = require('mogoose');
+const dateFormat = require('../utils/date-format');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtTime => dateFormat(createdAtTime)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtTime => dateFormat(createdAtTime)
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function() {
+    return this.reactions.length;
+})
+
+const Thought = model("Thought", ThoughtSchema);
+const Reaction = model("Reaction", ReactionSchema)
+
+module.exports = { Thought, Reaction }
